374384294099be04490d657a32fdf30766473045
Yeshwanth N <yeshwanthngs1r@gmail.com>
2026-07-10 15:16:14 +0530

    Updated variables

--- 01_fundamentals/02_variable.py
+++ 01_fundamentals/02_variable.py
@@ -1,3 +1,5 @@
-x = 121 #x is a variable, specifically integer variable
+x = 121 # x is a variable, specifically integer variable
 
 print(x)
+
+# A variable stores the value you assigned it, it can be for various data types
