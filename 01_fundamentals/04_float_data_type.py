0879e673fa6d0b7490110580d153b88259238642
Yeshwanth N <yeshwanthngs1r@gmail.com>
2026-07-11 13:26:54 +0530

    Learned Float data type

---
+++ 01_fundamentals/04_float_data_type.py
@@ -0,0 +1,14 @@
+# float: Basically fractional numbers and decimal numbers
+# again negative end to positive end
+# also precision isn't always great
+
+a = -10.5
+b = 0.0
+c = 10.5
+
+print("Flaot Data Type")
+print(a, b, c)
+print(type(a), type(b), type(c))
+print()
+print("-" * 50)
+print()
