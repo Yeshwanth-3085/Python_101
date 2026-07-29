83653556b0adbe882ae9ccb0c581bfc09426d8b2
Yeshwanth N <yeshwanthngs1r@gmail.com>
2026-07-29 19:24:30 +0530

    Learned String Methods: Case Methods

---
+++ 01_fundamentals/03_string_data_type/string_methods/01_string_case_methods.py
@@ -0,0 +1,40 @@
+# String Methods: Case Methods
+
+some_str = "Hello World!"
+
+print("\nupper(): Converts a string into uppercase.")
+print(f"some_str = {some_str}")
+print(f"some_str.upper() -> {some_str.upper()}")
+
+print()
+print("-" * 50)
+print()
+
+print("lower(): Converts a string into lowercase.")
+print(f"some_str = {some_str}")
+print(f"some_str.lower() -> {some_str.lower()}")
+
+print()
+print("-" * 50)
+print()
+
+print("title(): Capitalizes the first character of each word.")
+print(f'"hello world!".title() -> {"hello world!".title()}')
+
+print()
+print("-" * 50)
+print()
+
+print("capitalize(): Capitalizes the first character and converts the remaining characters to lowercase.")
+print(f"some_str = {some_str}")
+print(f"some_str.capitalize() -> {some_str.capitalize()}")
+
+print()
+print("-" * 50)
+print()
+
+print("swapcase(): Swaps uppercase letters to lowercase and vice versa.")
+print(f"some_str = {some_str}")
+print(f"some_str.swapcase() -> {some_str.swapcase()}")
+
+print()
